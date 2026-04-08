9236b335958ac33ab70b6a958aeabfb8bc53d507
Jitesh Krishna <jiteshkrishna06@gmail.com>
2026-04-08 20:15:37 +0530

    Phase 8 Completed

--- src/config.py
+++ src/config.py
@@ -1,5 +1,17 @@
 from pathlib import Path
 
+"""
+This module serves as the central location for configuration constants and file paths. 
+It ensures that various components of the project access consistent settings for data paths, 
+directories, model thresholds, and feature definitions. Maintaining all essential configurations 
+here makes the codebase easier to maintain and less error-prone.
+"""
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data" / "customer_churn.csv"
 MODEL_DIR = BASE_DIR / "model"
+
+NUMERIC_COLS = ['tenure', 'MonthlyCharges', 'TotalCharges']
+
+THRESHOLD = 0.35
--- src/predict.py
+++ src/predict.py
@@ -2,14 +2,36 @@ import pandas as pd
 import joblib
 import sys
 
-from src.config import MODEL_DIR
-from src.config import DATA_PATH
+from src.config import MODEL_DIR, THRESHOLD
 from src.preprocess import preprocess_inf
 
 def predict(data):
 
+    """
+    The predict function serves as a pipeline that takes in input data (as a Pandas DataFrame),
+    preprocesses it to match the feature set and format expected by the trained model, 
+    loads the trained model, and produces predictions. 
+
+    Specifically, it performs the following steps:
+    1. Loads a trained model from disk using joblib.
+    2. Preprocesses the input data using the preprocess_inf function to ensure
+       correct feature types, encodings, and scaling, matching what the model expects.
+    3. Removes the target variable 'Churn' if present to avoid data leakage.
+    4. Applies the model to generate predicted probabilities and classes for churn.
+    5. Returns both the predictions (as binary 0/1) and the corresponding probabilities.
+
+    This function enables seamless batch or single-row inference for new/unseen data.
+    """
+
+
     # Load Model
-    model = joblib.load(MODEL_DIR/"logistic_model.pkl")
+    model = joblib.load(MODEL_DIR/"model.pkl")
+    required_cols = joblib.load(MODEL_DIR/"model_columns.pkl")
+
+    # Adding all columns before preprocessing
+    for col in required_cols:
+        if col not in data.columns:
+            data[col] = 0
 
     # Preprocess
     df = preprocess_inf(data)
@@ -20,30 +42,6 @@ def predict(data):
 
     # Predict
     y_prob = model.predict_proba(df)[:, 1]
-    y_pred = (y_prob >= 0.35).astype(int)
+    y_pred = (y_prob >= THRESHOLD).astype(int)
 
     return y_pred, y_prob
-
-
-# Uncomment to test
-# run the below command to get results
-
-# python -m src.predict data/sample.csv 
-
-# replace data/sample.csv with your own data
-
-
-
-# yp, ypr = predict(pd.read_csv(sys.argv[1]))
-
-# results = pd.DataFrame({
-#     "Churn Prediction": yp,
-#     "Churn Probability": ypr
-# })
-
-# results["Churn Prediction"] = results["Churn Prediction"].map({
-#     0: "No",
-#     1: "Yes"
-# })
-
-# print(results.head(5))
